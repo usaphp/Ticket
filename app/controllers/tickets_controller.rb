92101a1d15aa38afe938ceb0b0998cfcc3defe54
Tamerlan Soziev <usaphp@gmail.com>
2012-02-23 06:50:19 -0500

    Initial commit

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,9 @@
 class TicketsController < ApplicationController
   def index
     @months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    
+    @js_class = "Ticket"
+    @js_functions = ["tickets_init()"]
   end
 
 end
